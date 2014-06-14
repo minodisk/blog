4863e7fc73943f2d6062a2ed53ddf84beed3d5c1
minodisk <daisuke.mino@gmail.com>
2014-06-15 02:43:29 +0900

    Updating site

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,7 +1,7 @@
 rm -rf public
 git subtree add --prefix public https://github.com/minodisk/minodisk.github.io.git master --squash
 git subtree pull --prefix=public
-hugo
+hugo --theme="hpstr"
 git add -A
 git commit -m "Updating site" && git push origin master
 git subtree push --prefix=public https://github.com/minodisk/minodisk.github.io.git master
